19a2d3575031a0549ac6a96675bb8c381c85c00b
NhanPhamTrong <ptnhan1201@gmail.com>
2022-03-31 02:05:27 +0700

    fix in App.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import Card from "./Card";
 import content from "../content";
 
-function CreateCard() {
+function CreateCard(content) {
     return (
         <Card
             key={content.id}
-            logo={content.logo}
+            image={content.logo}
             title={content.title}
             text={content.text}
         />
